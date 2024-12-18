9aa7c59d3fce34c60b6a6b550fc8151b2c029325
AI_intelligence_genZ <natthanathron@gmail.com>
2024-12-18 13:45:03 +0700

    Update pytorch_camera.py

--- code YOLOv8 GPU/pytorch_camera.py
+++ code YOLOv8 GPU/pytorch_camera.py
@@ -1,30 +1,13 @@
-import cv2  # OpenCV library
-from ultralytics import YOLO  # YOLO library
-
-# Load the YOLOv8 model
-model = YOLO("yolov8n.pt")  # Replace with your YOLO model path if different
-
-# Initialize video capture (0 for webcam, or replace with video file path)
-cap = cv2.VideoCapture(0)  # Use 'video.mp4' for file input
-
+import cv2
+from ultralytics import YOLO
+model = YOLO("yolov8n.pt")
+cap = cv2.VideoCapture(0)
 while cap.isOpened():
-    success, frame = cap.read()  # Capture frame
-
-    # Perform YOLO inference
+    success, frame = cap.read()
     results = model(frame)
-
-    # Annotate the frame with detection results
     annotated_frame = results[0].plot()
-
-    # Display the annotated frame
     cv2.imshow("YOLOv8 Inference", annotated_frame)
-
-    # Exit the loop when 'q' is pressed
     if cv2.waitKey(1) & 0xFF == ord("q"):
-        print("Exiting...")
         break
-
-# Release resources
 cap.release()
 cv2.destroyAllWindows()
-print("Resources released. Program terminated.")
