2ce8077394f2a1f3b129e9e3590a43eb8419bdd8
AI_intelligence_genZ <natthanathron@gmail.com>
2025-03-22 18:49:31 +0700

    Add files via upload

---
+++ code YOLOv8 Openvino/ultralytics/openvino_camera.py
@@ -0,0 +1,29 @@
+import cv2
+from ultralytics import YOLO
+import time
+import ipywidgets as widgets
+import openvino.runtime as ov
+model =  YOLO('yolov8n.pt') #can comment
+model.export(format='openvino',dynamic=True,imgsz=(640,640),half=False) #can comment
+core = ov.Core()
+device = widgets.Dropdown(
+    options=core.available_devices + ["GPU"],
+    value='GPU',
+    description='Device:',
+    disabled=False,
+)
+device
+ov_model = YOLO('yolov8n_openvino_model')
+cap = cv2.VideoCapture(0)
+while cap.isOpened():
+    success, frame = cap.read()
+    start_time = time.time()
+    results = ov_model(frame)
+    end_time = time.time()
+    print(f"Inference time: {end_time - start_time:.3f} seconds")
+    annotated_frame = results[0].plot()
+    cv2.imshow("YOLOv8 Inference", annotated_frame)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+cap.release()
+cv2.destroyAllWindows()
